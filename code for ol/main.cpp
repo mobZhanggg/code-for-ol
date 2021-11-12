8e09ee89c3bf38efbcdea1ae87b2f3f5cb0d572a
mobzhang <3115342881@qq.com>
2021-11-12 15:09:01 +0800

    Initial Commit

---
+++ code for ol/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code for ol
+//
+//  Created by mob on 2021/11/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
